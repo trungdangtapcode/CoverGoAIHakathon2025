b9956c34440fab2b524f1bf0061831ad2ec6c0cf
PhatNguyen <nguyenthuanphatvl@gmail.com>
2025-10-25 22:53:48 +0700

    Remove Log Files and beta versions

--- surfsense_web/components/sidebar/app-sidebar.tsx
+++ surfsense_web/components/sidebar/app-sidebar.tsx
@@ -213,7 +213,6 @@ export const AppSidebar = memo(function AppSidebar({
 								</div>
 								<div className="grid flex-1 text-left text-sm leading-tight">
 									<span className="truncate font-medium">Strawberries</span>
-									<span className="truncate text-xs">beta v0.0.8</span>
 								</div>
 							</div>
 						</SidebarMenuButton>
